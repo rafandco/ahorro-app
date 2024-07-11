9a0d9ec8df522bdba295366ae6d645af9ffa7624
Rafael Díaz <rafandpufff@gmail.com>
2024-07-11 20:25:48 +0200

    Seed db with development data

--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,45 @@
-import { db } from 'astro:db';
+import { db, User, Entry } from "astro:db"
 
 // https://astro.build/db/seed
 export default async function seed() {
-	// TODO
+  await db.insert(User).values([
+    {
+      id: 1,
+      userName: "fer07",
+      firstName: "Fernando",
+      lastName: "Rodríguez López",
+      password: "fernando1234",
+    },
+    {
+      id: 2,
+      userName: "fran09",
+      firstName: "Francisco",
+      lastName: "Pérez Ramírez",
+      password: "Fracisco1234",
+    },
+  ])
+
+  await db.insert(Entry).values([
+    {
+      id: 1,
+      initialAmount: 10,
+      finalAmount: 20,
+      createdAt: new Date(Date.now()),
+      userId: 1,
+    },
+    {
+      id: 2,
+      initialAmount: 23,
+      finalAmount: 28,
+      createdAt: new Date(Date.now()),
+      userId: 1,
+    },
+    {
+      id: 3,
+      initialAmount: 43,
+      finalAmount: 68,
+      createdAt: new Date(Date.now()),
+      userId: 2,
+    },
+  ])
 }
