d95d588d102d543cd541859d0dc7fb18ec5195f4
Rafael Díaz <rafandpufff@gmail.com>
2024-07-11 19:47:33 +0200

    Define tables in db/config.ts

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,28 @@
-import { defineDb } from 'astro:db';
+import { defineDb, defineTable, column, NOW } from "astro:db"
+
+// Tabla de usuarios
+const User = defineTable({
+  columns: {
+    id: column.number({ primaryKey: true }),
+    userName: column.text({ unique: true }),
+    firstName: column.text(),
+    lastName: column.text(),
+    password: column.text(),
+  },
+})
+
+// Tabla de entradas
+const Entry = defineTable({
+  columns: {
+    id: column.number({ primaryKey: true }),
+    initialAmount: column.number(),
+    finalAmount: column.number(),
+    createdAt: column.date({ default: NOW }),
+    userId: column.number({ references: () => User.columns.id }),
+  },
+})
 
 // https://astro.build/db/config
 export default defineDb({
-  tables: {}
-});
+  tables: { User, Entry },
+})
