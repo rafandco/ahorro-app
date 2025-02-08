9c9f2ff7cbeb721570940777ebc90d568d7b7138
Rafael Díaz <rafandpufff@gmail.com>
2025-02-08 22:30:02 +0100

    Update astrodb

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
-import { defineConfig } from "astro/config"
-import db from "@astrojs/db"
-import vercel from "@astrojs/vercel/serverless"
+import { defineConfig } from "astro/config";
+import db from "@astrojs/db";
+import vercel from "@astrojs/vercel/serverless";
+import vue from "@astrojs/vue";
 
-import vue from "@astrojs/vue"
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [db(), vue()],
   output: "server",
   adapter: vercel()
-})
+});
