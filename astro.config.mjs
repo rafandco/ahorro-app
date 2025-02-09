63ff8a16b550653c4292563aa52a601a49fd2405
Rafael Díaz <rafandpufff@gmail.com>
2025-02-09 01:40:07 +0100

    Migrate to Turso

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
-import { defineConfig } from "astro/config";
-import db from "@astrojs/db";
-import vercel from "@astrojs/vercel/serverless";
-import vue from "@astrojs/vue";
+// @ts-check
+import { defineConfig } from 'astro/config';
 
+import db from '@astrojs/db';
+
+import vue from '@astrojs/vue';
+
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [db(), vue()],
   output: "server",
-  adapter: vercel()
+  adapter: vercel(),
 });
